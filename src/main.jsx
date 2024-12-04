c5aae3ccd997eaa29486fd2c76f72cd2851e3876
umkhulubhungane <admin@Apple-MacBook-Pro-Retina-13-Early-2015.local>
2024-12-04 13:10:11 +0200

    Changed Favorites.jsx to Favourites.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App.jsx';
 import ShowDetails from './ShowDetails';
-import Favorites from './Favorites';
+import Favorites from './Favourites';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
